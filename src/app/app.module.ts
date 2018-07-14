22c29720f46f57604db965b27a049d90a350378a
Unnamed <31389643+UnnamedVip@users.noreply.github.com>
2018-07-14 19:37:52 +0500

    observable and promise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +18,8 @@ import { ThirdComponent } from './third/third.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/third/third.component.ts
+++ src/app/third/third.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-third',
@@ -10,6 +11,65 @@ export class ThirdComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.observableFunction();
+    this.promiseFunction();
   }
+
+  //#region for Observable
+  observablePush: any = [];
+  observableShow: any = [];
+  observableMessage: string;
+
+  observableFunction() {
+    this.getArrForObsPush().subscribe(data => { this.observableShow = data; });
+  }
+
+  setNewValue(val: string) {
+
+    this.observablePush.push(val);
+    this.observableMessage = '';
+  }
+
+  clearAr() {
+    this.observablePush = [];
+    this.observableFunction();
+  }
+
+  getArrForObsPush(): Observable<any[]> {
+    return of(this.observablePush);
+  }
+  //#endregion for Observeble
+
+
+  //#region for Promise
+  promisePush: any = [];
+  promiseShow: any = [];
+  promiseMessage: string;
+
+  
+  promiseFunction() {
+    this.getArrPromForPush().then(data => {
+      this.promiseShow = data;
+    });
+  }
+
+  setNewPromValue(val: string) {
+    this.promisePush.push(val);
+    this.promiseMessage = '';
+  }
+
+  clearPromAr() {
+    this.promisePush = [];
+    this.promiseFunction();
+  }
+
+  getArrPromForPush() {
+    let promise = new Promise(resolve => {
+      resolve(this.promisePush);
+    });
+    return promise;
+  }
+
+  //#endregion for Promise
 
 }

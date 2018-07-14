082093850fd6cc12d734e60b383e5da67448cfdb
Unnamed <31389643+UnnamedVip@users.noreply.github.com>
2018-07-14 12:43:58 +0500

    regExp form for testing

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-second',
@@ -7,16 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondComponent implements OnInit {
 
-  constructor() {
-    //console.log('second', 'Constructor Run');
+  private card: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.card = this.formBuilder.group({
+      companyName: ['', Validators.required]
+    });
+
+    this.card.valueChanges.subscribe(dataAll => {
+      this.checkcard(dataAll)
+    }, () => console.log('complete'));
   }
 
   ngOnInit() {
-    //console.log('second', 'ngOnInit Run');
   }
 
-  getSum(a:number, b:number){
+  getSum(a: number, b: number) {
     return a + b;
+  }
+
+  checkNameCompany:boolean=false;
+
+  checkcard(data) {
+    //let regVar =  /^[А-Я]{1}[а-я]{1,14}( [|А-Яа-я]{1})?([а-я]{0,14})?( [А-Яа-я]{1})?([а-я]{1,14})?$/;
+    let regVar = /(^([A-Z]{1}[a-z]+(\s[A-Z]{1}[a-z]+$|$)))|(^([А-я]{1}[а-я]+(\s[А-я]{1}[а-я]+$|$)))/;
+
+    if (regVar.test(data.companyName)) {
+      this.checkNameCompany = true;
+      console.log(true);
+    }
+    else {
+      this.checkNameCompany = false;
+      console.log(false);
+    }
   }
 
 }
